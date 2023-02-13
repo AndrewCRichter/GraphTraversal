a8884fdd24527e331d0760b574f0923dde2a10dd
Andrew <andrewrichter36@gmail.com>
2023-02-13 00:07:04 -0500

    Finished code!!!

--- src/Graph.java
+++ src/Graph.java
@@ -2,15 +2,17 @@
 
 public class Graph
 {
-    private int numNodes;
-    private Node nodes[];
-    private boolean unfound[];
-    Node searchQueueStart;
-    Node searchQueueEnd;
+    private final int numNodes;
+    private final Node[] nodes;
+    private final boolean[] unfound;
+    private Node start;
+    private boolean breadth;
+
 
     public Graph()
     {
         this(8);
+        this.addConnection(1, 2);
         this.addConnection(1, 3);
         this.addConnection(2, 1);
         this.addConnection(2, 3);
@@ -40,15 +42,13 @@ public class Graph
         this.numNodes = numNodes;
         this.nodes = new Node[numNodes];
         this.unfound = new boolean[numNodes];
-        for(int i = 0; i < numNodes; ++i)
-        {
-            unfound[i] = true;
-        }
     }
     public void addConnection(int startID, int endID)
     {
-        if(startID < numNodes && endID < numNodes)
+        if(startID <= numNodes && endID <= numNodes && startID > 0 && endID > 0)
         {
+            startID--;
+            endID--;
             Node next = nodes[startID];
             Node last = null;
             while(next != null)
@@ -63,7 +63,7 @@ public class Graph
             }
             if(last == null)
             {
-                nodes[startID].setNext(new Node(endID));
+                nodes[startID] = new Node(endID);
             }
             else
             {
@@ -73,6 +73,82 @@ public class Graph
         else
         {
             System.out.println("Node dose not exists.");
+        }
+    }
+    public void Search(int startNode, boolean breadthFirst)
+    {
+        if(startNode <= numNodes && startNode > 0)
+        {
+            for(int i = 0; i < numNodes; ++i)
+            {
+                unfound[i] = true;
+            }
+            startNode--;
+            breadth = breadthFirst;
+            start = new Node(startNode);
+            Node current = start;
+            Node lastAdded = start;
+            Node connection;
+            unfound[startNode] = false;
+            while(current != null)
+            {
+                connection = nodes[current.getID()];
+                while (connection != null)
+                {
+                    if(unfound[connection.getID()])
+                    {
+                        unfound[connection.getID()] = false;
+                        if(breadth)
+                        {
+                            lastAdded.setNext(new Node(connection.getID()));
+                            lastAdded = lastAdded.getNext();
+                        }
+                        else
+                        {
+                            lastAdded = new Node(connection.getID());
+                            lastAdded.setNext(current.getNext());
+                            current.setNext(lastAdded);
+                        }
+                    }
+                    connection = connection.getNext();
+                }
+                current = current.getNext();
+            }
+        }
+        else
+        {
+            System.out.println("Start Node dose not exists.");
+        }
+    }
+    public void Display()
+    {
+        if(start != null)
+        {
+            Node visited = start;
+            String out;
+            if(breadth)
+            {
+                out = "BFS";
+            }
+            else
+            {
+                out = "DFS";
+            }
+            out += " with the start node " + (start.getID() + 1) + " discovers the nodes in the following order: ";
+            while (visited != null)
+            {
+                out += visited.getID() + 1;
+                visited = visited.getNext();
+                if(visited == null)
+                {
+                    out += ".\n\n";
+                    System.out.println(out);
+                }
+                else
+                {
+                    out += ", ";
+                }
+            }
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Graph graph = new Graph();
+        Scanner in = new Scanner(System.in);
+        int inputInt;
+        String inputString;
+        boolean again = true;
+        boolean breadth = true;
+        while (again)
+        {
+            System.out.println("Enter a start node number for the node traversal: ");
+            inputInt = in.nextInt();
+            in.nextLine();
+            System.out.println("Enter B for breadth-first search or D for depth-first search: ");
+            do {
+                again = false;
+                inputString = in.nextLine().strip();
+                if (inputString.equalsIgnoreCase("B"))
+                {
+                    breadth = true;
+                }
+                else if (inputString.equalsIgnoreCase("D"))
+                {
+                    breadth = false;
+                }
+                else
+                {
+                    System.out.println("Pleas enter B or D: ");
+                    again = true;
+                }
+            } while (again);
+            graph.Search(inputInt, breadth);
+            graph.Display();
+            System.out.println("Would you like to go again? Enter Y if so and anything else to exit: ");
+            inputString = in.nextLine().toUpperCase();
+            again = inputString.equalsIgnoreCase("Y");
+        }
     }
 }
